a946b28f57332001db776283cff8424e18e46dcf
vengadanathan srinivasan <vengadanathansrinivasan@vengadanathans-MacBook-Pro-2.local>
2024-09-24 19:52:38 +0530

    updating sql command

--- init.sql
+++ init.sql
@@ -2,11 +2,12 @@ CREATE TABLE IF NOT EXISTS emails (
     email_id VARCHAR PRIMARY KEY,
     email_content TEXT,
     email_subject TEXT,
-    original_email_subject TEXT,
-    original_email_from_address TEXT,
-    original_email_text TEXT,
-    attachments TEXT[],
     to_address VARCHAR,
     status VARCHAR CHECK (status IN ('PENDING', 'APPROVED', 'REJECTED')),
     created_date TIMESTAMP
 );
+
+ALTER TABLE IF EXISTS emails
+    ADD COLUMN IF NOT EXISTS original_email_subject TEXT,
+    ADD COLUMN IF NOT EXISTS original_email_from_address TEXT,
+    ADD COLUMN IF NOT EXISTS original_email_text TEXT;
